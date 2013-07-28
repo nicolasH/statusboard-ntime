1536345f66807f2686e18731454abe7571939c48
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2013-07-28 20:44:37 +0200

    Centered the status images in their cells.

--- appleDev/my_html.py
+++ appleDev/my_html.py
@@ -1,12 +1,12 @@
 def to_simple_row(name, status):
-    return """<tr><td class="noresize" width=45px"><img src="%s.png"></td><td>%s</td></tr>""" % (status,name)
+    return """<tr><td class="noresize" width="45px"><center><img src="%s.png"></center></td><td>%s</td></tr>""" % (status,name)
 
 def to_img_row(status_a, status_b, status_c):
     return """
     <tr>
-        <td class="noresize" width=45px"><img src="%s.png"></td>
-        <td class="noresize" width=45px"><img src="%s.png"></td>
-        <td class="noresize" width=45px"><img src="%s.png"></td>
+        <td class="noresize" width="45px"><center><img src="%s.png"></center></td>
+        <td class="noresize" width="45px"><center><img src="%s.png"></center></td>
+        <td class="noresize" width="45px"><center><img src="%s.png"></center></td>
     </tr>""" % (status_a, status_b, status_c)
 
 
